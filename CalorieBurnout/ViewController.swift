1f15a0b4c04a9f0b6b54e88f35824e57ff770a55
Ashwin Gattani <gattani.ashwin@gmail.com>
2019-09-25 13:01:41 +0530

    Initial Commit

---
+++ CalorieBurnout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalorieBurnout
+//
+//  Created by Ashwin Gattani on 25/09/19.
+//  Copyright © 2019 Ashwin Gattani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
